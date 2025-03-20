8d8fce7a982cb38a6e61698ffd4e748547e05979
jakubbrom <jbrom@lanres.cz>
2025-03-20 16:27:56 +0100

    Changes in variables definition.

--- static/libs/AIHABs.py
+++ static/libs/AIHABs.py
@@ -1,8 +1,8 @@
 # Imports
-from get_S2_points_OpenEO import authenticate_OEO, get_s2_points_OEO
-from calculate_features import calculate_feature
-from get_meteo import getHistoricalMeteoData, getPredictedMeteoData
-from data_imputation import data_imputation
+from .get_S2_points_OpenEO import get_s2_points_OEO
+from .calculate_features import calculate_feature
+from .get_meteo import getHistoricalMeteoData, getPredictedMeteoData
+from .data_imputation import data_imputation
 
 class AIHABs:
 
@@ -31,17 +31,21 @@ class AIHABs:
         """
 
         # Authenticate after starting the program
-        authenticate_OEO()          # The OpenEO needs to be authenticated first
+        self.provider_id = "CDSE"
+        self.client_id = None
+        self.client_secret = None
+        self.oeo_backend = "https://openeo.dataspace.copernicus.eu"
 
         self.db_name = "postgres"
         self.user = "postgres"
-        self.db_table_reservoirs = "water_reservoirs"
+        self.db_table_reservoirs = "reservoirs"
         self.db_table_points = "selected_points"
         self.db_table_S2_points_data = "s2_points_eo_data"  # db_bands_table
         self.db_features_table = "wq_points_results"
         self.db_models = "models_table"
         self.db_table_forecast = "meteo_forecast"
         self.db_table_history = "meteo_history"
+        self.db_access_date = "last_access"
 
         self.model_name = None,
         self.default_model = False
@@ -53,14 +57,14 @@ class AIHABs:
                       "shortwave_radiation_sum"]
 
         self.freq = 'D'
-        self.t_shift = 1
+        self.t_shift = 3
         self.forecast_days = 16
 
 
     def run_analyse(self):
 
         # get Sentinel-2 data
-        get_s2_points_OEO(self.osm_id, self.db_name, self.user, self.db_table_reservoirs, self.db_table_points, self.db_table_S2_points_data)
+        get_s2_points_OEO(self.provider_id, self.client_id, self.client_secret, self.osm_id, self.db_name, self.user, self.db_table_reservoirs, self.db_table_points, self.db_table_S2_points_data, self.db_access_date, oeo_backend_url=self.oeo_backend)
 
         # calculate WQ features --> new AI models
         model_id = calculate_feature(self.feature, self.osm_id, self.db_name, self.user, self.db_table_S2_points_data, self.db_features_table, self.db_models, model_name=self.model_name, default=self.default_model)[1]
@@ -72,12 +76,13 @@ class AIHABs:
         getPredictedMeteoData(self.osm_id, self.meteo_features, self.user, self.db_name, self.db_table_forecast, self.db_table_reservoirs, self.forecast_days)
 
         # imputation of missing values (based on SVR model)
-        if model_id is not None:
-            gdf_imputed, gdf_smooth = data_imputation(self.db_name, self.user, self.osm_id, self.feature, model_id, self.db_features_table, self.db_table_history, freq=self.freq, t_shift=self.t_shift)
-        else:
-            gdf_imputed = None
-            gdf_smooth = None
-        # run AI time series analysis
+        # if model_id is not None:
+        #     gdf_imputed, gdf_smooth = data_imputation(self.db_name, self.user, self.osm_id, self.feature, model_id, self.db_features_table, self.db_table_history, freq=self.freq, t_shift=self.t_shift)
+        # else:
+        #     gdf_imputed = None
+        #     gdf_smooth = None
+        # # run AI time series analysis
 
-        return gdf_imputed, gdf_smooth
+        # return gdf_imputed, gdf_smooth
+        return
 
