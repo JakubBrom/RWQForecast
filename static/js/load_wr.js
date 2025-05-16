0184dca81894395e2e05c9d1ebca6d53ba75ede8
jakubbrom <jbrom@lanres.cz>
2025-05-16 13:39:07 +0200

    Loading of water reservoirs in the map updated

--- static/js/load_wr.js
+++ static/js/load_wr.js
@@ -1,4 +1,4 @@
-//Funkcionalita mapy
+// Funkcionalita mapy
 var myMap = L.map('map_result').setView([48.8, 14.8], 5);
 window.myMap = myMap;
 
@@ -7,39 +7,72 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
-let selectedLayer = null;
+function addInteractiveGeoLayer(options) {
+    const {
+        map,
+        geoJsonUrl,
+        inputSelector,
+        style = { color: "blue", weight: 2 },
+        highlightStyle = { color: "red", weight: 2 },
+        getId = f => f.properties.osm_id,
+        getPopup = f => `Name: ${f.properties.name || "Noname"}<br>OSM_ID: ${f.properties.osm_id || "?"}<br>Area: ${f.properties.area || "?"}`
+    } = options;
 
-var waterBodiesLayer = L.geoJSON(null, {
-    style: function () {
-        return { color: "blue", weight: 2};
-    },
-    onEachFeature: function (feature, layer) {
-        layer.bindPopup("Name: " + (feature.properties.name || "Noname") + "<br>" +
-                        "OSM_ID: " + (feature.properties.osm_id || null) + "<br>" +
-                        "Area: " + (feature.properties.area || null) + " ha"
-        );
+    let selectedLayer = null;
+    let geoJsonData = null;
+    let userTriggered = true;
 
-        layer.on('click', function () {
-            if (selectedLayer) {
-                waterBodiesLayer.resetStyle(selectedLayer);
-            }
-            selectedLayer = this;
-            this.setStyle({ color: "red" });
-
-            document.getElementById("sel_wr").value = feature.properties.osm_id;
-        });
+    const inputEl = document.querySelector(inputSelector);
+    if (!inputEl) {
+        console.error(`Input feature '${inputSelector}' not found.`);
+        return;
     }
-}).addTo(myMap);
 
-// Dynamické načítání GeoJSON dat
-function loadWaterBodies() {
-        waterBodiesLayer.clearLayers();
-        var currentZoom = myMap.getZoom();
-        console.log("Aktuální zoom úroveň:", currentZoom);
-        var geojsonData = null;
+    const layerGroup = L.geoJSON(null, {
+        style: () => style,
+        onEachFeature: function (feature, layer) {
+            const id = getId(feature);
+            layer.bindPopup(getPopup(feature));
 
-        // Získání aktuálního rozsahu mapy
-        const bounds = myMap.getBounds();
+            layer.on('click', function () {
+                highlightFeature(layer, id);
+            });
+        }
+    }).addTo(map);
+
+    function highlightFeature(layer, id) {
+        if (selectedLayer) {
+            layerGroup.resetStyle(selectedLayer);
+        }
+        selectedLayer = layer;
+        layer.setStyle(highlightStyle);
+        inputEl.value = id;
+    }
+
+    inputEl.addEventListener("change", function () {
+        const val = this.value;
+        if (!geoJsonData) return;
+
+        let found = false;
+        layerGroup.eachLayer(function (layer) {
+            const feature = layer.feature;
+            if (feature && String(getId(feature)) === val) {
+                userTriggered = false;
+                map.fitBounds(layer.getBounds());
+                highlightFeature(layer, getId(feature));
+                setTimeout(() => userTriggered = true, 500);  // Obnovíme po půl vteřině
+                found = true;
+            }
+        });
+
+        if (!found) {
+            alert(`Feature ID ${val} not found.`);
+        }
+    });
+
+    function loadData() {
+        layerGroup.clearLayers();
+        const bounds = map.getBounds();
         const bbox = {
             south: bounds.getSouth(),
             west: bounds.getWest(),
@@ -47,18 +80,43 @@ function loadWaterBodies() {
             east: bounds.getEast()
         };
 
-        fetch('/add_wr_to_map', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(bbox)
-        })
-        .then(response => response.json())
-        .then(data => {
+        const dataPromise = typeof geoJsonUrl === "function" ? geoJsonUrl(bbox) : fetch(geoJsonUrl).then(r => r.json());
+
+        dataPromise.then(data => {
             geoJsonData = data;
-            L.geoJSON(data, {}).addTo(myMap);
-        console.log("Data načtena:", data.elements);
-        waterBodiesLayer.addData(data);    
+            layerGroup.addData(data);
+            console.log("Načtena data:", data);
+        });
+    }
+
+    map.on("moveend", () => {
+        if (userTriggered) {
+            loadData();
+        }
     });
+
+    loadData();
+
+    return {
+        layer: layerGroup,
+        reload: loadData
+    };
 }
-myMap.on("moveend", loadWaterBodies);
-loadWaterBodies();
+
+// Pokud #sel_wr neexistuje, potom použijeme #sel_wr_sp
+var selWr = "#sel_wr";
+if (!document.querySelector(selWr)) {
+    selWr = "#sel_wr_sp";
+}
+
+addInteractiveGeoLayer({
+    map: myMap,
+    geoJsonUrl: (bbox) => fetch('/add_wr_to_map', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(bbox)
+    }).then(r => r.json()),
+    inputSelector: selWr,
+    getId: f => f.properties.osm_id,
+    getPopup: f => `Name: ${f.properties.name || 'Noname'}<br>OSM ID: ${f.properties.osm_id}<br>Area: ${f.properties.area}`
+});
