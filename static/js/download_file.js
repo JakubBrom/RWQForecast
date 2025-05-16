b37aa41c7814b9806b402437f6ce2bd57104320a
jakubbrom <jbrom@lanres.cz>
2025-05-16 13:34:15 +0200

    Script updated

--- static/js/download_file.js
+++ static/js/download_file.js
@@ -1,14 +1,14 @@
 // Frontend JS funkce pro předání dat a stažení souboru
 
 // Get data from the forms and download the file
-function downloadData(downlBtn, downloadUrl) {
+function downloadData(downlBtn, downloadUrl, selWr, selectWq, datePicker, selModel) {
     $(document).ready(function () {
         $(downlBtn).click(function () {
-            const osm_id = $('#sel_wr').val();
-            const feature = $('#select_wq').val();
-            const date = $('#datepicker').val();
-            const model_id = $('#sel_model').val();
-            const wr_name = $('#sel_wr option:selected').text();
+            const osm_id = $(selWr).val();
+            const feature = $(selectWq).val();
+            const date = $(datePicker).val();
+            const model_id = $(selModel).val();
+            const wr_name = $(selWr + ' option:selected').text();
 
             // Sestavení URL s parametry
             const url = `/${downloadUrl}?osm_id=${encodeURIComponent(osm_id)}&feature=${encodeURIComponent(feature)}&date=${encodeURIComponent(date)}&model_id=${encodeURIComponent(model_id)}&wr_name=${encodeURIComponent(wr_name)}`;
@@ -19,5 +19,5 @@ function downloadData(downlBtn, downloadUrl) {
     });
 }
 
-downloadData('#downl-vect', 'download_gpkg')
-downloadData('#downl-ts', 'download_ts')
+downloadData('#downl-vect', 'download_gpkg', '#sel_wr_sp', '#select_wq_sp', '#datepicker', '#sel_model_sp')
+downloadData('#downl-ts', 'download_ts', '#sel_wr', '#select_wq', '#datepicker', '#sel_model')
